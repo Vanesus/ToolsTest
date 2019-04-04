2cebff7830e5d550cf915ed857bc751897ae7397
Silvano Gil <sgil@pronoide.es>
2019-04-04 10:42:31 +0200

    Update App.java

--- src/main/java/com/toolstest/App.java
+++ src/main/java/com/toolstest/App.java
@@ -8,7 +8,21 @@ public class App {
     private String name;
 
     public static void main(String[] args) {
+        //TODO hacer algo con este proyecto
         name = "Fer";
         System.out.println(name);
+    }
+    
+    public void metodo()
+    {
+        if(true){
+            if(true){
+                if(true){
+                    if(true){
+                        System.out.println(name);
+                    }
+                }
+            }    
+        }
     }
 }
